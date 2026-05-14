ec500e7966cff2ff8fa08cfd783eed0aa84b305d
Nezha6106 <scannnprint@gmail.com>
2026-05-14 10:28:44 +0530

    Auto-run course population script

--- build.sh
+++ build.sh
@@ -5,3 +5,6 @@ python -m pip install -r requirements.txt
 mkdir -p logs
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+# Add this line right here!
+python manage.py populate_courses
