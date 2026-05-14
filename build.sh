94dfc4e9600fa45e52ec32a14842ffaf48616eae
Nezha6106 <scannnprint@gmail.com>
2026-05-15 03:32:02 +0530

    Auto-create live superuser

--- build.sh
+++ build.sh
@@ -6,5 +6,11 @@ mkdir -p logs
 python manage.py collectstatic --no-input
 python manage.py migrate
 
+# Auto-create a superuser for the live server
+export DJANGO_SUPERUSER_USERNAME=admin
+export DJANGO_SUPERUSER_EMAIL=admin@admin.com
+export DJANGO_SUPERUSER_PASSWORD=adminpassword123
+python manage.py createsuperuser --noinput || true
+
 # Add this line right here!
 python manage.py populate_courses
